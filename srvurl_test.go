fd2e679cd3d68ad7b2e7cdfe7f0953ddc0a34870
Sheldon Hearn <sheldonh@starjuice.net>
2014-10-23 17:06:07 +0200

    Add some nonsense cases

--- srvurl_test.go
+++ srvurl_test.go
@@ -3,9 +3,16 @@ package srvul
 import "testing"
 
 var expectations = map[string]string{
-	"http://www.starjuice.net/":           "http://www.starjuice.net/",
-	"http://_test._tcp.starjuice.net/":    "http://localhost.localdomain:3000/",
+	"":                                    "",
+	"file:///":                            "file:///",
+	"http://127.0.0.1:8080":               "http://127.0.0.1:8080",
+	"https://www.starjuice.net/":          "https://www.starjuice.net/",
 	"http://_test._tcp.starjuice.net:80/": "http://_test._tcp.starjuice.net:80/",
+	"http://_test._tcp.starjuice.net:/":   "http://_test._tcp.starjuice.net:/",
+	"http://_test._tcp.starjuice.net/":    "http://localhost.localdomain:3000/",
+	"http://www.starjuice.net:8080/":      "http://www.starjuice.net:8080/",
+	"http://www.starjuice.net/":           "http://www.starjuice.net/",
+	":/:nonsense/:!":                      ":/:nonsense/:!",
 }
 
 func TestUrlResolver(t *testing.T) {
